0c36b4bb85c16b9feeff1425ca42d5166c7c2bd6
Philippe Paris <contacto@philippeparis.net>
2021-03-14 12:39:26 -0300

    update

---
+++ parts/form-controll.php
@@ -0,0 +1,71 @@
+<?php
+
+// grab recaptcha library
+require_once "recaptchalib.php";
+// your secret key
+$secret = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe";
+// empty response
+$response = null;
+// check secret key
+$reCaptcha = new ReCaptcha($secret);
+$name = $email = $subject = $phone = $message = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  $name = test_input($_POST["name"]);
+  $email = test_input($_POST["email"]);
+  $phone = test_input($_POST["phone"]);
+  $subject = test_input($_POST["subject"]);
+  $message = test_input($_POST["message"]);
+  $response = $reCaptcha->verifyResponse(
+      $_SERVER["REMOTE_ADDR"],
+      $_POST["g-recaptcha-response"]
+  );
+
+}
+
+function test_input($data)
+{
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+// Cambios solicitados acá
+function get_ip_address()
+{
+  foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key) {
+    if (array_key_exists($key, $_SERVER) === true) {
+      foreach (explode(',', $_SERVER[$key]) as $ip) {
+        $ip = trim($ip); // just to be safe
+
+        if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false) {
+          return $ip;
+        }
+      }
+    }
+  }
+}
+// Fin de cambios
+
+
+$mailTo = "contacto@philippeparis.net";
+$headers = "From: " . $email;
+$txt = "Formulario de " . $name . ".\n\nTeléfono " . $phone . "\n\n" . $message;
+
+mail($mailTo, $subject, $txt, $headers);
+// Cambios solicitados acá
+$namelog = date("Ymd") . ".csv";
+$ip = get_ip_address();
+$ua = $_SERVER['HTTP_USER_AGENT'];
+$hora = date("d-m-Y H:i:s");
+
+$arr = array($hora, $ip, $ua, $name, $phone, $email, $subject, $message);
+$contacto = implode("\t", $arr) . "\n";
+file_put_contents($namelog, $contacto, FILE_APPEND | LOCK_EX);
+// Fin de cambios
+header("Location: gracias.html");
+echo "Hi " . $_POST["name"] . " (" . $_POST["email"] . "), thanks for submitting the form!";
+
+?>
--- parts/form.php
+++ parts/form.php
@@ -1,110 +1,4 @@
-<?php
-
-// grab recaptcha library
-require_once "recaptchalib.php";
-
-
-// your secret key
-$secret = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe";
-
-// empty response
-$response = null;
-
-// check secret key
-$reCaptcha = new ReCaptcha($secret);
-
-
-
-
-
-$name = $email = $subject = $phone = $message = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-
-  $name = test_input($_POST["name"]);
-  if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
-    $nameErr = "Sólo letras y espacios permitidos";
-  }
-
-
-  $email = test_input($_POST["email"]);
-  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $emailErr = "Formateo invalido";
-  }
-
-
-
-  $phone = test_input($_POST["phone"]);
-  if (!filter_var($phone, FILTER_VALIDATE_EMAIL)) {
-    $phoneErr = "Formateo de teléfono invalido";
-  }
-
-
-
-  $subject = test_input($_POST["subject"]);
-
-
-
-  $message = test_input($_POST["message"]);
-
-  // if submitted check response
-  if ($_POST["g-recaptcha-response"]) {
-    $response = $reCaptcha->verifyResponse(
-      $_SERVER["REMOTE_ADDR"],
-      $_POST["g-recaptcha-response"]
-    );
-  }
-}
-
-function test_input($data)
-{
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-
-// Cambios solicitados acá
-function get_ip_address()
-{
-  foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key) {
-    if (array_key_exists($key, $_SERVER) === true) {
-      foreach (explode(',', $_SERVER[$key]) as $ip) {
-        $ip = trim($ip); // just to be safe
-
-        if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false) {
-          return $ip;
-        }
-      }
-    }
-  }
-}
-// Fin de cambios
-
-
-$mailTo = "contacto@philippeparis.net";
-$headers = "From: " . $email;
-$txt = "Formulario de " . $name . ".\n\nTeléfono " . $phone . "\n\n" . $message;
-
-mail($mailTo, $subject, $txt, $headers);
-// Cambios solicitados acá
-$namelog = date("Ymd") . ".csv";
-$ip = get_ip_address();
-$ua = $_SERVER['HTTP_USER_AGENT'];
-$hora = date("d-m-Y H:i:s");
-
-$arr = array($hora, $ip, $ua, $name, $phone, $email, $subject, $message);
-$contacto = implode("\t", $arr) . "\n";
-file_put_contents($namelog, $contacto, FILE_APPEND | LOCK_EX);
-// Fin de cambios
-header("Location: gracias.html");
-echo "Hi " . $_POST["name"] . " (" . $_POST["email"] . "), thanks for submitting the form!";
-
-?>
-
-
-<form class="contact-form recaptchaForm" action="parts/form.php" method="post">
+<form class="contact-form recaptchaForm" action="parts/form-controll.php" method="post">
   <div class="section section-contact-us text-center" id="contacto">
     <div class="container">
       <h2 class="title">Cotiza con nosotros</h2>
@@ -133,7 +27,7 @@ echo "Hi " . $_POST["name"] . " (" . $_POST["email"] . "), thanks for submitting
                 <!-- <i class="now-ui-icons users_circle-08"></i> -->
               </span>
             </div>
-            <input class="form-control" type="number" name="phone" placeholder="56 9 1234 5678" min="10" max="11" placeholder="Teléfono" required>
+            <input class="form-control" type="number" name="phone" placeholder="56 9 1234 5678" placeholder="Teléfono" required>
           </div>
           <div class="input-group input-lg">
             <div class="input-group-prepend">
