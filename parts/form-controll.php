ca459300685a382cf28317dcd2168d7d783b4d51
Philippe Paris <contacto@philippeparis.net>
2021-03-15 00:16:44 -0300

    update

--- parts/form-controll.php
+++ parts/form-controll.php
@@ -51,10 +51,6 @@ if ($_POST) {
 
   // Fin de cambios
 
-  header("Location: gracias.php");
+/*   header("Location: gracias.php"); */
 
-  if(isset($email)){
-    echo 'Success! Thanks for submitting';
- }
- 
 }
--- parts/gracias.php
+++ parts/gracias.php
@@ -3,5 +3,7 @@
     <div class="">
         <h2>Gracias por enviarnos</h2>
     </div>
+
+    <a href="../">volver</a>
 </section>
 <?php include('layout/footer.php') ?>
